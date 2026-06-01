db38f460a1a40e98a9d24f6168e4f42c58aaeffa
harsha <hreshavena@gmail.com>
2026-06-01 16:15:03 +0530

    export_import example updated completed

--- export_import/app.js
+++ export_import/app.js
@@ -1,4 +1,7 @@
 import va from "./server.js"
 
-console.log(va)
+import {arr} from "./server.js"
 
+
+console.log(va)//10
+console.log(arr)//[1,2,3,4,5]
--- export_import/server.js
+++ export_import/server.js
@@ -2,5 +2,6 @@ const a= 10
 
 const user = "krishna"
 
+export const arr = [1,2,3,4,5]
 
 export default user

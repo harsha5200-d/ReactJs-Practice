1017cf9a2e7918092b4d6e89ce5fa5a50bb6c61c
harsha <hreshavena@gmail.com>
2026-06-02 11:02:17 +0530

    understanding_props

---
+++ understanding_props/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./index.css"
+
+
+const App = () =>{
+  return (
+    <div className='parent'>
+
+      <div className="card"> 
+        <h1> Hare Krishna </h1>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
